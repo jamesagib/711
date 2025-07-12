12b24a2c31f2110f42a38732512b7ae6ed33211d
Mark Agib <80441909+MarkAppprogrammer@users.noreply.github.com>
2025-07-12 16:14:10 -0700

    add more data

--- 7-11/classifer/test_data/main.py
+++ 7-11/classifer/test_data/main.py
@@ -1,15 +1,42 @@
 import json
 
-file_name = 'classifer/test_data/test.json'
+#file_name = 'classifer/test_data/test.json'
 
-with open(file_name, 'r') as f:
-    data = json.load(f)
+def remove_duplicates(file_name):
+    with open(file_name, 'r') as f:
+        data = json.load(f)
 
-for i in range(len(data) - 1, 0, -1):
-    if data[i]['title'] == data[i - 1]['title']:
-        data.pop(i)
+    data.sort(key=lambda x: x['title'])
+    for i in range(len(data) - 1, 0, -1):
+        if data[i]['title'] == data[i - 1]['title']:
+            data.pop(i)
 
-with open(file_name, 'w') as f:
-    json.dump(data, f, indent=4, ensure_ascii=False, sort_keys=True)
+    with open(file_name, 'w') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False, sort_keys=True)
 
+def append_data(file_name, temp_file_name):
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    with open(temp_file_name, 'r') as f:
+        temp_data = json.load(f)
+    data.extend(temp_data)
+    with open(file_name, 'w') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False, sort_keys=True)
 
+if __name__ == "__main__":
+    print("Options:")
+    print("1. Remove duplicates")
+    print("2. Append data")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        file_name = input("Enter the file name: ")
+        remove_duplicates(file_name)
+    elif choice == "2":
+        file_name = "classifer/test_data/data.json"
+        temp_file_name = input("Enter the file name: ")
+        append_data(file_name, temp_file_name)
+    elif choice == "3":
+        exit()
+    else:
+        print("Invalid choice")
